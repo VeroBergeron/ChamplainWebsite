b212510d88735bd8b4cd055499bf69af0e9d248d
tom leb <tomlebreux@hotmail.com>
2016-01-26 20:08:05 -0500

    Implement read from file for About page

---
+++ App_Code/JsonHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using System.Web.Script.Serialization;
+
+
+/// <summary>
+/// Summary description for JsonHandler
+/// </summary>
+public class JsonHandler
+{
+    public JsonHandler()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+
+    public static String fromFile(String filename)
+    {
+        String dataFolder = Path.Combine(HttpContext.Current.Request.PhysicalApplicationPath, "App_Data");
+        StreamReader sr = new StreamReader(dataFolder + "/" +filename);
+        String jsonString = sr.ReadToEnd();
+        return jsonString;
+    }
+
+    public static dynamic toJson(String jsonString)
+    {
+        JavaScriptSerializer js = new JavaScriptSerializer();
+        dynamic json = js.Deserialize<dynamic>(jsonString);
+        return json;
+    }
+}
